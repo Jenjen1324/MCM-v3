9e5fd87e1baf2310c6e8ad5aab8e25741c66773d
Jenjen1324 <northcodedotno@gmail.com>
2013-12-18 15:43:04 +0100

    ..

---
+++ mcm.mc/MinecraftVersion/ReleaseType.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCM.MC.MinecraftVersion
+{
+    /// <summary>
+    /// Specifies the Release Type of a Minecraft version
+    /// </summary>
+    public enum ReleaseType
+    {
+        release,
+        snapshot,
+        old_beta,
+        old_alpha,
+        modified,
+        unknown
+    }
+}
