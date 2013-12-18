aaa5811eb093531a3bfa05e30aecee88ad92c4f9
Jenjen1324 <northcodedotno@gmail.com>
2013-12-18 14:37:40 +0100

    Started again...

---
+++ mcm.core/DownloadManager/DownloadManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCM.Core.DownloadManager
+{
+    /// <summary>
+    /// The Manager that starts the downloads
+    /// </summary>
+    public class DownloadManager
+    {
+        /// <summary>
+        /// Schedules a single file to be downloaded
+        /// </summary>
+        /// <param name="dl">The download to be downloaded</param>
+        public void ScheduleDownload(Download dl);
+
+        /// <summary>
+        /// Schedules the download of all downloads in the Package
+        /// </summary>
+        /// <param name="dl">The Package to be downloaded</param>
+        public void ScheduleDownload(DownloadPackage dl);
+    }
+}
---
+++ mcm.core/DownloadManager/DownloadPackage.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCM.Core.DownloadManager
+{
+    /// <summary>
+    /// A Package that contains multiple downloads but are for one cause
+    /// </summary>
+    public class DownloadPackage : Download
+    {
+
+
+        /// <summary>
+        /// The Downloads the be downloaded
+        /// </summary>
+        public List<Download> files;
+
+        /// <summary>
+        /// Downloads
+        /// </summary>
+        public void Download();
+    }
+}
